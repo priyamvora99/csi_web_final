27c97769c6b0f64a8953ac4e8338d43b12e6a32f
Priyam Vora <priyamvora@Priyams-MacBook-Pro.local>
2020-01-27 17:16:25 +0530

    Changes to server.js

--- server.js
+++ server.js
@@ -4,4 +4,10 @@ const path = require('path');
 const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 app.listen(port);
